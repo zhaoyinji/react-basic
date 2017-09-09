7d50bc5b5726f55d4a3e6ea487f2f7570c984267
zhaoyinji <zhaoyinji@gmail.com>
2017-09-09 23:12:11 +0300

    First Hello world app

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-console.log('work');
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>Hello world!</h1>
+            </div>
+        );
+    }
+}
+
+render(<App/>, window.document.getElementById('app'));
